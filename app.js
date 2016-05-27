788b6aca8cc516faff08beb9c85c5f7f84b8b2de
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-05-27 22:47:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,6 +87,8 @@ function initDBConnection() {
 		dbCredentials.user = "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix";
 		dbCredentials.password = "e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4";
 		dbCredentials.url = "https://b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix:e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4@b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com";
+	    
+		cloudant = require('cloudant')(dbCredentials.url);
 	}
 }
 
