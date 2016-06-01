02462fb899bbfe43389220592779c9af3bbb7175
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-01 14:07:49 +0530

    Add Get Mfg years api

--- controllers/usage.js
+++ controllers/usage.js
@@ -4,8 +4,8 @@ var express = require('express');
 var config = require('../models/usage.js');
 var helper = require('./helpers/utility.js');
 
-module.exports.getUsageParams = function(req, res, next) {
-	config.getUsageParams(function(err, result){  	  
+module.exports.getUsage = function(req, res, next) {
+	config.getUsage(function(err, result){  	  
 	    helper.sendResponse(res, err, result); 
 	});
 };
