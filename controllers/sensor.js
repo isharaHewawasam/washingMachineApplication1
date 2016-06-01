db7bef9916f49ace39d1deb9bd7bc5f99ec69db3
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-01 17:39:19 +0530

    Add Get Sensors api

---
+++ controllers/sensor.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var express = require('express');
+var sensor = require('../models/sensor.js');
+var helper = require('./helpers/utility.js');
+
+module.exports.getUsage = function(req, res, next) {
+	sensor.getUsage(function(err, result){  	  
+	    helper.sendResponse(res, err, result); 
+	});
+};
