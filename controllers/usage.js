ca40c464daea601f4911cf4f2f49707f8a1d3208
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-01 14:47:04 +0530

    Add Get Usage api

--- controllers/usage.js
+++ controllers/usage.js
@@ -1,11 +1,11 @@
 "use strict"
 
 var express = require('express');
-var config = require('../models/usage.js');
+var usage = require('../models/usage.js');
 var helper = require('./helpers/utility.js');
 
 module.exports.getUsage = function(req, res, next) {
-	config.getUsage(function(err, result){  	  
+	usage.getUsage(function(err, result){  	  
 	    helper.sendResponse(res, err, result); 
 	});
 };
--- models/usage.js
+++ models/usage.js
@@ -2,19 +2,28 @@
 var db = require('../database/db');
 var COLLECTION_NAME = 'stores';
 
-exports.Usage = function(callback) {
+exports.getUsage = function(callback) {
 	  var opts = { q: '*:*', counts: ['MfgYear'], limit:0 };  
 	  
 	  db.search('yearsByMfgDate', 'yearsByMfgDate', opts, function(err, result) {
 	    if(err) {
 	    	callback(err, null)
 	    } else {  
-		  var all_mfg_years = {'years': []};
-			
-		  for(var year in result.counts.MfgYear) {	  	
-			  all_mfg_years.years.push({'year': year});
-		  }	
-		
+		  var all_mfg_years = {'data': [ {"make": "LG",
+                                       "model": "Woo1",
+                                       "totalLoads" : 10,
+                                       "popularDay": "Monday",
+                                       "popularTime": "12:00 - 13:00"                                     
+                                      },
+                                      {"make": "Whirlpool",
+                                       "model": "Fuzzy - 10",
+                                       "totalLoads" : 15,
+                                       "popularDay": "Monday",
+                                       "popularTime": "14:00 - 15:00"                                     
+                                      },
+                                    ]
+                          };        
+		  	
 	      callback(err, all_mfg_years);
 	    }
 	  });
