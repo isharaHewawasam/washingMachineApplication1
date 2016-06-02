0c57ac43b2d351db84901612a609161c8e555747
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-02 13:27:27 +0530

    Add get sales chart api

---
+++ controllers/sales.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var express = require('express');
+var usage = require('../models/sales.js');
+var helper = require('./helpers/utility.js');
+
+module.exports.getCharts = function(req, res, next) {
+	usage.getCharts(function(err, result){  	  
+	    helper.sendResponse(res, err, result); 
+	});
+};
