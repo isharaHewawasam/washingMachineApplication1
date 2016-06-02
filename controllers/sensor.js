8a716c7fd838a7faf251cb2e02948506b4f4451d
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-02 12:52:11 +0530

    Add updated UI code

--- controllers/sensor.js
+++ controllers/sensor.js
@@ -9,3 +9,9 @@ module.exports.getUsage = function(req, res, next) {
 	    helper.sendResponse(res, err, result); 
 	});
 };
+
+module.exports.getUsageByAttribute = function(req, res, next) {
+	sensor.getUsage(function(err, result){  	  
+	    helper.sendResponse(res, err, result); 
+	});
+};
