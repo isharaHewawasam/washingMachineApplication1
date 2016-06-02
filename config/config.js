6ce1dac8081eefa608bdd73ebcc6edc5f20498ff
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-02 17:26:46 +0530

    Update response and request payload for get sales by chart api

---
+++ config/config.js
@@ -0,0 +1,16 @@
+//Dont modify this file
+//Copy this file as config and modify when deployed to new server and then start the app
+//also dont chk modifed file in repo
+module.exports = {
+  "SalesDatabase": {
+    "provider": "Cloudant",
+	  "host": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
+	  "port": 443,
+	  "user": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix",
+	  "Password": "e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4",
+	  "url": "https://b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix:e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4@b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
+	  "dbName": "erpsalesdata"
+   },
+   
+   "host": "localhost:3000"
+};
