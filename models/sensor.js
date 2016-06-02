2d03035719722bc974a7df2f7abb7c43652f20b5
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-06-02 12:36:35 +0530

    Add get washing attributes by sensor API

--- models/sensor.js
+++ models/sensor.js
@@ -38,3 +38,40 @@ exports.getUsage = function(callback) {
 	    }
 	  });
 	};
+  
+  exports.getUsageByAttribute = function(callback) {
+	  var opts = { q: '*:*', counts: ['MfgYear'], limit:0 };  
+	  
+	  db.search('yearsByMfgDate', 'yearsByMfgDate', opts, function(err, result) {
+	    if(err) {
+	    	callback(err, null)
+	    } else {  
+		  var all_mfg_years = {'data': [ { "make": "LG",
+                                       "model": "Woo1",
+                                       "avgWaterUsed" : 10.5,
+                                       "avgEngeryUsed": 4.55,
+                                       "avgWashCycleDuration": 2,
+                                       "avgWashCycles": 55,
+                                       "avgTemperature": 55.66,
+                                       "avgDetergentUsed" : 10.55,
+                                       "avgMoisture": 70.09,
+                                       "avgWeight": 50.66                                        
+                                      },
+                                      { "make": "Whirlpool",
+                                       "model": "Fuzzy",
+                                       "avgWaterUsed" : 3.5,
+                                       "avgEngeryUsed": 48.55,
+                                       "avgWashCycleDuration": 2,
+                                       "avgWashCycles": 55,
+                                       "avgTemperature": 55.66,
+                                       "avgDetergentUsed" : 10.55,
+                                       "avgMoisture": 70.09,
+                                       "avgWeight": 50.66                                        
+                                      },
+                                    ]
+                          };        
+		  	
+	      callback(err, all_mfg_years);
+	    }
+	  });
+	};
