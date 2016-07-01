33bab14961dc7267094abea4e76e46226034fb1d
Asheni <asheni_silva@accelerite.com>
2016-07-01 09:54:12 +0530

    squash commit of following
    Updated the service credentials for cloudant
    
    Updated the memory of the app

--- config/config.js
+++ config/config.js
@@ -7,23 +7,23 @@ module.exports = {
   "cloud-host": "ibm-iot.mybluemix.net",
   "SalesDatabase": {
      "provider": "Cloudant",
-	   "host": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
+	   "host": "9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix.cloudant.com",
 	   "port": 443,
-	   "user": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix",
-	   "Password": "e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4",
-	   "url": "https://b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix:e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4@b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
-	  "dbName": "erpsalesdata"
+	   "user": "9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix",
+	   "Password":  "829646b74b4d31bb4719ff5de13cfd765db6750a567d526282a5a30263c57cd8",
+	   "url": "https://9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix:829646b74b4d31bb4719ff5de13cfd765db6750a567d526282a5a30263c57cd8@9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix.cloudant.com",
+	   "dbName": "erpsalesdata"
    },
    "WashDailyAggregateDatabase": {
      "provider": "Cloudant",
-	   "host": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
+	   "host": "9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix.cloudant.com",
 	   "port": 443,
-	   "user": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix",
-	   "Password": "e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4",
-	   "url": "https://b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix:e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4@b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
-	   "dbName": "newupdate1000data",
-     "dbName_1lac": "devicedatanew",
-     "dbName_1k": "newupdate1000data"
+	   "user": "9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix",
+	   "Password": "829646b74b4d31bb4719ff5de13cfd765db6750a567d526282a5a30263c57cd8",
+	   "url": "https://9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix:829646b74b4d31bb4719ff5de13cfd765db6750a567d526282a5a30263c57cd8@9aba8573-52a8-43f0-9b18-cab8a0b35343-bluemix.cloudant.com",
+	   "dbName": "washdatadailyaggregation2",
+     "dbName_1lac": "washdatadailyaggregation2",
+     "dbName_1k": "washdatadailyaggregation2"
    },
    "RegionLatituesLongitudes": {
      "provider": "Cloudant",
