0b1a26a45791912628428c3d8d976f10b0f15467
Dilendra Sajini <dilendra_sajini@persistent.co.in>
2016-07-04 10:00:26 +0530

    removed dummy api

--- models/faults.js
+++ models/faults.js
@@ -2,25 +2,25 @@
 var db = require('../database/db');
 var COLLECTION_NAME = 'stores';
 
-//exports.getCommonFaults = function(callback) {
-//  var opts = { q: '*:*', counts: ['State'], limit:0 };  
-//  
-//  db.search('all-states', 'allStates', opts, function(err, result) {
-//    if(err) {
-//    	callback(err, null)
-//    } else {  
-//	  var all_states = {'states': []};
-//		
-//	  for(var state in result.counts.State) {	  	
-//	    all_states.states.push({'name': state});
-//	  }	
-//	
-//      callback(err, all_states);
-//    }
-//  });
-//};
-
 exports.getCommonFaults = function(callback) {
-	  callback(null, require("./commonfaultsdummy").commonFaults);  
+  var opts = { q: '*:*', limit:1 };  
+  
+  db.search('all-states', 'allStates', opts, function(err, result) {
+    if(err) {
+    	callback(err, null)
+    } else {  
+	  var all_states = {'states': []};
+		
+	  for(var state in result.counts.State) {	  	
+	    all_states.states.push({'name': state});
+	  }	
+	
+      callback(err, all_states);
+    }
+  });
 };
+
+//exports.getCommonFaults = function(callback) {
+//	  callback(null, require("./commonfaultsdummy").commonFaults);  
+//};
 
