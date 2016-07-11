4a9aba5cb2405da7d4990e950cfbf2a3139fd4fd
Atul Shimpi <atul_shimpi@persistent.co.in>
2016-07-11 10:03:55 +0530

    Point UI to bluemix endpoint

--- models/insights/most_used_products.js
+++ models/insights/most_used_products.js
@@ -6,54 +6,66 @@ exports.getData = function(payload, callback) {
   var FilterModule = require("../filters");
   var KeyMap = require("../view_keys_mapping");
   
+  validatePayload(payload);
   var Filter = new FilterModule(payload, 1);
   var key_map = new KeyMap();
   
-  //if (Filter.isFilterCategoryByYear()) {
-   // key_map.setReportType2SensorByYear();
-  //} else {
-    
-  //}
-  
-  key_map.setReportType2MostUsedWM();
-  
+  if (Filter.isFilterCategoryByYear()) {
+    key_map.setReportType2SensorByYear();
+  } else {
+    key_map.setReportType2Sensor();
+  }
+ 
   var averagesBuffer = [];
+  
   var params = { 
-                 "description": "Most Used",
+                 "description": "Most Used Products for " + payload.timescale.years[0].value,
                  "payload": payload,
                  "buffer": averagesBuffer,
                  "view": {
                            "designDocName": "insights",
-                           "default": "insights_dev",
-                           "byYear": "insights_dev"
+                           "default": "MostUsedByYear",
+                           "byYear": "MostUsedByYear"
                          },
                   "statsKeyName": "totalLoadWeight",
                   "databaseType": "sensorDailyAggregate",
                   "filter": Filter,
                   "key_maps": key_map,
-                  "top": 5,
-                  "start_key": getStartKey(),
-                  "end_key": getEndKey(),
-                  "group_level": 5
+                  "group_level_": 1
                };
   
   avg.getSum(params, function(err, result) {
-    callback(err, result.slice(0, 3));
+    result.sort(function(a, b) {
+      return b.totalLoadWeight - a.totalLoadWeight;
+    });
+    
+    var response = {};
+    
+    response.description = params.description;
+    response.data = result.slice(0, 3);
+    
+    for (var each_item in response.data) {
+      delete response.data[each_item].sold;
+      delete response.data[each_item].time_scale;
+    }
+    callback(err, response);
   });    
 };
 
-function getStartKey(){
-  var  result;
-  
-  result = ['2015', '10', '1']
-  
-  return result;
+function validatePayload(payload){
+  addYear(payload)
 }
 
-function getEndKey(){
-  var  result;
+function addYear(payload) {
+  if (payload.timescale === undefined || payload.timescale === null) {
+    payload.timescale = {};
+  }
   
-  result = ['2015', '10', '7']
+  if (payload.timescale.years === undefined || payload.timescale.years === null) {
+    payload.timescale.years = [];  
+  }
   
-  return result;
+  if (payload.timescale.years.length == 0 ) {
+    payload.timescale.years.push({"value": (new Date).getFullYear()});
+  }
 }
