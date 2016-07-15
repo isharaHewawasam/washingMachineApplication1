d51bc38579ddb1e4ebb60c5e9178c002d1fdc40c
Ewantha Kularathne <ewantha_kularathne@accelerite.com>
2016-07-15 17:10:10 +0530

    adding changes to pick current time in not config: oshanthis changes

--- UI/WebContent/app/js/notificationconfiguration.js
+++ UI/WebContent/app/js/notificationconfiguration.js
@@ -33,6 +33,19 @@ App.controller('NotificationConfController',
 				$state.go('app.singleview');
 			}
 		}
-		
+			var monthNames = [
+			  "January", "February", "March",
+			  "April", "May", "June", "July",
+			  "August", "September", "October",
+			  "November", "December"
+					];
+			
+					var date = new Date();
+					var day = date.getDate();
+					var monthIndex = date.getMonth();
+					var year = date.getFullYear();
+			
+			   
+			   $scope.currentDate=monthNames[monthIndex] + ' ' + day + ', ' +  year+ ', ' +date.toLocaleTimeString();
 		
 }]);
