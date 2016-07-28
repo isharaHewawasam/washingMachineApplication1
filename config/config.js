78956ed3ca15b3b71a55d442b0dc335ea594d911
Yasitha Dehigama <yasitha_dehigama@persistent.co.in>
2016-07-28 09:34:11 +0530

    Changed config/config file

--- config/config.js
+++ config/config.js
@@ -14,8 +14,8 @@ module.exports = {
 	   "url": "https://b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix:e0cddb23f5b90a6f590053fb3504e18dd3d6081354d53b6ec0a1c9afcc1054d4@b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
 	  "dbNameOld": "erpsalesdata",
     "dbName": "erpsaleswithstatecode"
-    
    },
+   
    "WashDailyAggregateDatabase": {
      "provider": "Cloudant",
 	   "host": "b1611753-b3bf-47ce-95f7-0c67cc1a61a7-bluemix.cloudant.com",
