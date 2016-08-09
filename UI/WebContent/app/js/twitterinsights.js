d3dba42aa8ec0b5a69e1533b231d0c5b75c01850
Amani Cooray <amani_cooray@persistent.co.in>
2016-08-09 10:29:08 +0530

    removing comments in twitterinsights.js

--- UI/WebContent/app/js/twitterinsights.js
+++ UI/WebContent/app/js/twitterinsights.js
@@ -1,36 +1,36 @@
-App.controller('TwitterInsightsController', 
+App.controller('TwitterInsightsController',
 		['$rootScope', '$scope', '$state', '$http', '$localStorage', "iot.config.ApiClient",
                  function($rootScope, $scope, $state, $http, $localStorage, configApiClient){
-	
+
 	$scope.showPerSign = false;
-	
+
 	$scope.twitter_insights_griddata = [];
-	
+
 	$("#gridAdjustHeight").height(400);
-	
+
 	$http({url:configApiClient.baseUrl + "twitter/innerpage/twitter-insights", //api url
         method: "GET",
         Accept: "text/plain"}).success(function(data, status) {
-        		$scope.twitter_insights_griddata = data.data; 
-          
+        		$scope.twitter_insights_griddata = data.data;
+
            }). error(function(data, status) {
       });
-	
+
 	$scope.editTwitterInsights = function(make, model) {
 		$localStorage.showTwitterInnerLint = true;
 		var twitterinsights = {};
 		twitterinsights.make = make;
 		twitterinsights.model = model;
 		$localStorage.twitterinsights = twitterinsights;
-		$state.go('app.notificationconf');	
+		$state.go('app.notificationconf');
 	};
-	
+
 	$scope.deleteTwitterInsights = function() {
 		alert("deleteTwitterInsights");
 	};
-	
+
 	$scope.tweetsData = {};
-	
+
 	$scope.loadTwitterInsightsData = function (id) {
 		var url = "twitter/innerpage/twittersentiments/"+id;
 		$http({url:configApiClient.baseUrl + url, //api url
@@ -41,51 +41,51 @@ App.controller('TwitterInsightsController',
 	        			getTwitterSentiments('twitter_sentiments', $scope.tweetsData);
 	        			$scope.showPerSign = true;
 	        		}
-		        	
+
 	           }). error(function(data, status) {
-	        	
-	             
+
+
 	    });
-		
-	};	
-	
+
+	};
+
 	$scope.loadTwitterInsightsData('');
-	
+
 	$scope.days = [{"day": "5", "desc": "Last 5 Days"}, {"day": "10", "desc": "Last 10 Days"}];
-	
+
 	$scope.loadDashboard = function() {
 		$rootScope.isApplyFiterButton = true;
 		$state.go('app.singleview');
 	};
-	
+
 	$scope.getTextColor = function(color){
 		return (color=='red'? 'twitter_percentage twitter_arrow_red' : 'twitter_percentage twitter_arrow_green');
 	}
-	
-	$scope.maximizeGrid=function(){		
+
+	$scope.maximizeGrid=function(){
 		var gridNormal = $("#gridNormal").clone();
-		
+
 		$("#gridMax").empty();
 		$("#gridMax").append(gridNormal);
-		
+
 		$("#gridMax #gridMaxImg").addClass("hidden");
 		$("#gridMax #gridCloseImg").removeClass("hidden");
-		//$("#gridCloseImg").removeClass("hidden");
-		
+
+
 		$("#gridAdjustHeight").height(560);
 
 	$("#gridMax").removeClass("hidden");
-	
+
 	};
-	
+
 	$("body").on("click","#gridCloseImg",function(){
 		$("#gridMax").empty();
 		$("#gridMax").addClass("hidden");
 		  $("#gridAdjustHeight").height(400);
     });
-	
-	
-	  
+
+
+
 	$scope.getTwitterData = function(divId) {
 		  var data = [
 		  			[1247529600000,20.32],
@@ -139,33 +139,33 @@ App.controller('TwitterInsightsController',
 			  			];
 		  }
 		  renderTwitterSentimentsLineChart(divId, data);
-    	
+
 	  }
-		
+
 		$scope.selectedRowIndex = -1;
-		
+
 		$scope.getTwitterInsightsData = function(id, index){
-			
+
 			$scope.loadTwitterInsightsData(id)
 			$scope.selectedRowIndex = index;
-		} 
+		}
 }]);
 
 function getTwitterSentiments (divId, twitter_sentiments) {
-	  
+
 	 var positive 	= twitter_sentiments.sentimate_positive;
 	 var pos_other	= (100-positive);
 	 var negative 	= twitter_sentiments.sentimate_neutral;
 	 var neg_other	= (100-negative);
 	 var neutral 	= twitter_sentiments.sentimate_negative;
 	 var neu_other	= (100-neutral);
-	 
+
 	 sentimentsData(divId+'_pos', 'Positive', positive, pos_other);
-	 
+
 	 sentimentsData(divId+'_neu', 'Neutral', neutral, neu_other);
-	 
+
 	 sentimentsData(divId+'_neg', 'Negative', negative, neg_other);
-	 
+
 	 // when browser resize need to redraw the chart to display the inner text at proper location
 	 $(window).resize(function() {
 		    if(this.resizeTO) clearTimeout(this.resizeTO);
@@ -174,20 +174,20 @@ function getTwitterSentiments (divId, twitter_sentiments) {
 		    	$(this).trigger('resizeEnd', objArr);
 		    }, 500);
 	  });
-	  
+
 	  $(window).bind('resizeEnd', function(event, param1) {
-		 
+
 		  sentimentsData(divId+'_pos', 'Positive', positive, pos_other);
-			 
+
 		  sentimentsData(divId+'_neu', 'Neutral', neutral, neu_other);
-			 
+
 		  sentimentsData(divId+'_neg', 'Negative', negative, neg_other);
-		  
+
 	  });
  };
- 
+
 function sentimentsData (divId, name, yValue, oValue) {
-	
+
 	 var twitterData	= [];
 	 var obj 			= {};
 	 obj.name 			= name;
@@ -197,10 +197,10 @@ function sentimentsData (divId, name, yValue, oValue) {
 	 obj.name 			= "Others";
 	 obj.y				= oValue
 	 twitterData.push(obj);
-	 
-	 var innerText = "<div width='100%' style='text-align:center'>"+yValue+"%</div><div>"+name+"<div>"; //data.centerText;
-	 renderTwitterSentimentsPieChart(divId, twitterData, innerText); 
-	 
+
+	 var innerText = "<div width='100%' style='text-align:center'>"+yValue+"%</div><div>"+name+"<div>";
+	 renderTwitterSentimentsPieChart(divId, twitterData, innerText);
+
 }
 
 function renderTwitterSentimentsLineChart(divId, insightsData) {
@@ -209,8 +209,7 @@ function renderTwitterSentimentsLineChart(divId, insightsData) {
 	        	renderTo: divId,
 	        	width: 140,
 	        	height:70,
-//	        	margin: [0, 10, 10, 10]
-	        	spacingLeft: 0,
+          	spacingLeft: 0,
 	        	spacingTop: 0
 	      },
 	      credits: {
@@ -249,16 +248,16 @@ function renderTwitterSentimentsLineChart(divId, insightsData) {
 	  });
 }
 function renderTwitterSentimentsPieChart(divId, insightsData, innerText){
-	
+
 	var colorCode = ['#339933', '#808080']; // For Positive
 	if(divId == 'twitter_sentiments_neg'){
 		colorCode = ['#FF0000', '#808080']; // For Negative
 	}else if(divId == 'twitter_sentiments_neu'){
 		colorCode = ['#5DADE2', '#808080']; // For Neutral
 	}
-	
-	
-	
+
+
+
 	var chart1 = new Highcharts.Chart({
         chart: {
         	renderTo:divId,
@@ -268,21 +267,18 @@ function renderTwitterSentimentsPieChart(divId, insightsData, innerText){
             plotBorderWidth: null,
             plotShadow: false,
             type: 'pie',
-            /*marginRight:140,*/
             margin: [0, 0, 0, 0],
             events: {
             	load: function(event) {
-            		/*if (divId == 'twitter-handle-container'){
-            			$('.highcharts-legend-item').last().append('<br><br><div style="font-size:12px; font-family:Lucida Sans Unicode; width:200px"><b>Comments-' +this.series[0].data[0].totalComments + '</b></div>');
-            		}*/
+
             	}
             }
         },
         credits: {
 	    	enabled: false
 	    },
-	    exporting: { 
-	    	enabled: false 
+	    exporting: {
+	    	enabled: false
 	    },
         title: {
             text: '',
@@ -296,7 +292,7 @@ function renderTwitterSentimentsPieChart(divId, insightsData, innerText){
             x: 15
         },
         tooltip: false,
-        
+
         plotOptions: {
             pie: {
                 allowPointSelect: true,
@@ -305,12 +301,12 @@ function renderTwitterSentimentsPieChart(divId, insightsData, innerText){
                     enabled: false
                 },
                 showInLegend: true,
-                colors: colorCode, //divId != 'twitter-handle-container'?['#339933', '#808080', '#ffcc00', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']: ['#5DADE2', '#D6EAF8'],
+                colors: colorCode,
                 size: 10,
                 center: ['50%', '50%']
             }
         },
-        
+
         series: [{
             name: 'Brands',
             colorByPoint: true,
@@ -321,27 +317,27 @@ function renderTwitterSentimentsPieChart(divId, insightsData, innerText){
         }]
     },
     function(chart) { // on complete
-    	
+
     	var div = '#'+divId;
-    	
+
     	var position = $(div).position();
-    	
-    	  
-        var textX = position.left + (chart.plotWidth  * 0.4) - 4; // chart.plotLeft + (chart.plotWidth  * 0.5);
-        var textY = position.top + (chart.plotHeight * 0.5); //chart.plotTop  + (chart.plotHeight * 0.5);
-        
+
+
+        var textX = position.left + (chart.plotWidth  * 0.4) - 4;
+        var textY = position.top + (chart.plotHeight * 0.5); 
+
         var span = "<div id='"+divId+"_info_text'>"+innerText+"</div>";
-        
+
         	$('#'+divId+'_info_text').remove();
 	        $('#'+divId+'_text').append(span);
 	        	span = $('#'+divId+'_text');
-	        	
-	        	
-	            
+
+
+
 	        	span.css('left', textX + (span.width() * -0.5)+30);
 	        	span.css('top', textY + (span.height() * -0.5)+2);
-       
+
     	}
-	
-    );	
+
+    );
 };
