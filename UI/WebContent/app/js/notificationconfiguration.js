ebd8496e1409058bb25ffb555b31534122d0d32d
Amani Cooray <amani_cooray@persistent.co.in>
2016-08-09 10:27:29 +0530

    removing comments in notificationconfiguration.js

--- UI/WebContent/app/js/notificationconfiguration.js
+++ UI/WebContent/app/js/notificationconfiguration.js
@@ -1,96 +1,56 @@
-App.controller('NotificationConfController', 
+App.controller('NotificationConfController',
 		['$rootScope', '$scope', '$state', '$http', '$window', '$localStorage', "iot.config.ApiClient",
                  function($rootScope, $scope, $state, $http, $window, $localStorage, configApiClient){
 
-		
+
 
 		var twitterinsights = $localStorage.twitterinsights;
-		
-		
+
+
 		$scope.showTwitterInsightsLink = $localStorage.showTwitterInnerLint;
 		$scope.notificationConfig = {};
-		/*
-		//delete $localStorage.twitterinsights;
-		if(twitterinsights) {
-			
-			$scope.selectedMake = twitterinsights.make;
-			$scope.selectedModel = twitterinsights.model;
-			
-			$http({url:configApiClient.baseUrl + 'config/makes', 
-			     method: "GET", Accept: "text/plain"}).success(function(data, status) {
-			         $scope.makes = data.makes;
-			         $scope.selectedMake = twitterinsights.make;
-			         
-		    }). error(function(data, status) {
-		      // alert("error"  +status);
-		      
-		    });
-			
-			
-			$http({url:configApiClient.baseUrl + 'config/makes/models?make_names='+$scope.selectedMake, 
-			     method: "GET", Accept: "text/plain"}).success(function(data, status) {
-			    	$scope.models = data[$scope.selectedMake];
-			    	$scope.selectedModel = twitterinsights.model;	           
-		    }). error(function(data, status) {
-			      // alert("error"  +status);
-			     
-		    });
-		}
-		
-		$scope.getModels = function(make){
-			$scope.selectedMake = make.selectedMake;
-			$scope.selectedModel = '';
-			$http({url:configApiClient.baseUrl + 'config/makes/models?make_names='+$scope.selectedMake, 
-			     method: "GET", Accept: "text/plain"}).success(function(data, status) {
-			    	$scope.models = data[$scope.selectedMake];
-		    }). error(function(data, status) {
-			      // alert("error"  +status);
-			      
-		    });
-	     }
-		
-		*/
-		
+
+
 		$scope.scores = [{"value" : 10}, {"value" : 20}];
 		$scope.baselines = [{"value" : 30}, {"value" : 40}];
-		
+
 		$scope.tolernces = [{"value" : 10}, {"value" : 20}];
-		
+
 		$scope.washErrors = [{"value" : 10}, {"value" : 20}];
-		
+
 		var loginCredentails = angular.fromJson($window.sessionStorage.loginCredentails);
 		var rolename = loginCredentails.Role;
 		var roleKey 	= loginCredentails.roleKey;
-		
+
 		$scope.isEngManager = (roleKey == 'eng_manager'?true:false);
-		
+
      // Session handling- used to display respective views for managers.
 		$scope.openMyDashboard = function () {
-			 
-			if($scope.isEngManager){				
+
+			if($scope.isEngManager){
 				$state.go('app.engmanagerview');
 			}else{
 				$state.go('app.singleview');
 			}
-			
+
 		}
-		$http({url:configApiClient.baseUrl + 'config/makes', 
-			method: "GET", Accept: "text/plain"}).success(function(data, status) {     
-			 $scope.makes=data.makes;		           
+		$http({url:configApiClient.baseUrl + 'config/makes',
+			method: "GET", Accept: "text/plain"}).success(function(data, status) {
+			 $scope.makes=data.makes;
 			}). error(function(data, status) {
-			// alert("error"  +status);
 			
+
 			});
 
          // Retrieve model names for respective make
-			$scope.selectedMake=function(){ 			
+			$scope.selectedMake=function(){
 			$rootScope.search.selectedModel="";
-			$http({url:configApiClient.baseUrl + 'config/makes/models?make_names='+$rootScope.search.selectedMake, 
-			method: "GET", Accept: "text/plain"}).success(function(data, status) {				
-				
-			 $scope.models=data[$rootScope.search.selectedMake];	           
+			$http({url:configApiClient.baseUrl + 'config/makes/models?make_names='+$rootScope.search.selectedMake,
+			method: "GET", Accept: "text/plain"}).success(function(data, status) {
+
+			 $scope.models=data[$rootScope.search.selectedMake];
 			}). error(function(data, status) {
-			
+
 			});
 
 			}
@@ -100,26 +60,26 @@ App.controller('NotificationConfController',
 			  "August", "September", "October",
 			  "November", "December"
 					];
-			
+
 					var date = new Date();
 					var day = date.getDate();
 					var monthIndex = date.getMonth();
 					var year = date.getFullYear();
-			
-			   
+
+
 			   $scope.currentDate=monthNames[monthIndex] + ' ' + day + ', ' +  year+ ', ' +date.toLocaleTimeString();
-		
+
      // Return back to Twitter Insights page
 		$scope.loadTwitterInsights = function() {
 			$state.go('app.twitterinsights');
-			
+
 		}
-		
+
 	// Save new notification configurations.
 		$scope.saveNotificationConfig = function() {
 			var requestBody;
-			var redirecto;			
-			
+			var redirecto;
+
 			if($scope.isEngManager){
 				requestBody = {
 						"UserName": loginCredentails.username,
@@ -128,7 +88,7 @@ App.controller('NotificationConfController',
 						"IncreaseErrortype2": "Sensor",
 						"DecreaseErrortype2": "Sensor"
 				};
-				
+
 				redirecto = 'app.engmanagerview';
 			}else{
 				requestBody = {
@@ -144,20 +104,20 @@ App.controller('NotificationConfController',
 				};
 				redirecto = 'app.twitterinsights';
 			}
-			$http({url:configApiClient.baseUrl + 'notifications/configurations/settings/frompage', 
-			     method: "POST", 
+			$http({url:configApiClient.baseUrl + 'notifications/configurations/settings/frompage',
+			     method: "POST",
 			     headers: { 'Content-Type': 'application/json'},
 			     data: requestBody})
 			     .success(function(data, status) {
 			    	 $state.go(redirecto);
 		    }). error(function(data, status) {
-		    	
+
 		    });
-			
+
 		}
     // Cancel the Set up notification tolerances and Route to Twitter Insights page
 		$scope.cancelNotificationConfig = function() {
-			
+
 			if($scope.isEngManager){
 				$state.go('app.engmanagerview');
 			}else{
