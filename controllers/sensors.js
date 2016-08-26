0a1a4ff59bf594392bf6347a8ea91c4e7310869b
Yasitha Dehigama <yasitha_dehigama@persistent.co.in>
2016-08-26 14:05:58 +0530

    Fixed Defect 5299

--- controllers/sensors.js
+++ controllers/sensors.js
@@ -18,7 +18,14 @@ module.exports.getSensorsAvgUsage = function(req, res, next) {
   var sensor_name = req.swagger.params.sensor_name === undefined ? null : req.swagger.params.sensor_name.value;  
 	var group_by_timescale = req.swagger.params.group_by_timescale === undefined ? false : req.swagger.params.group_by_timescale.value;
   
-  sensors.getSensorsAvgUsage(sensor_name, group_by_timescale, req.body, function(err, result){  	  
+  sensors.getSensorsAvgUsage(sensor_name, group_by_timescale, req.body, function(err, result){  
+  		for(var i=0;i<result.length;i++){
+  			for(var j=0;j<result[i].avgUsage.length;j++){
+  				if(result[i].avgUsage[j]>0){
+  					result[i].avgUsage[j]=Math.round(result[i].avgUsage[j] * 100) / 100;
+  				}
+  			}
+  		}	  
 	    helper.sendResponse(res, err, result);
 	});
 };
