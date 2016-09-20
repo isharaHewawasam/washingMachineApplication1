0e76e466944f822de3c517ab8305bd30b01b2ac6
Aloka Munasinghe <aloka_munasinghe@persistent.co.in>
2016-09-20 18:32:21 +0530

    Fixing issue 6361: Mkt/Eng- Manager Data Grid Table Title Columns and Data columns are distorted in firefox

--- UI/WebContent/app/app.module.js
+++ UI/WebContent/app/app.module.js
@@ -14,6 +14,7 @@
 	          'ngResource', 
 	          'ui.utils', 
 	          'ngMaterial',
-	          'ngMessages'
+	          'ngMessages',
+	          'anguFixedHeaderTable'
 	      ]);
 })();
